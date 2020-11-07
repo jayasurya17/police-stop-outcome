4f8ce9e356b88771dcc227b81cc32dbc9b732ca6
duvvurisrihari <duvvurisrihari@gmail.com>
2020-11-07 03:26:32 -0800

    Visualization of column 1 to 4

--- project.py
+++ project.py
@@ -5,27 +5,7 @@ from sklearn.model_selection import train_test_split
 def read_file(filename):
   	return pd.read_csv(filename)
 
-def transform_data_columns_1_4(dataframe):
-	#The county name column can be dropped cause it is completely null
-	dataframe=dataframe.drop(columns=['county_name'], axis=1, inplace=False)
 
-	# The gender dataset contains nulls. Here those nulls are discarded cause 
-	# the dataset is large and the nulls consitute about 5%. Also replacing the
-	# null data with frequent values might mislead the prediction
-	dataframe = dataframe.dropna(subset=['driver_gender'], inplace=False)
-  
-	return dataframe
-   
-    
-def visualize_data_columns_1_4(dataframe):
-  
-	#The statistics of the first four coumns are as follows
-
-	#Column 1 -> Stop Date - The date on which the person is stopped
-
-	#Column 3 -> Gender 
-	print(dataframe['driver_gender'].value_counts())
-	return dataframe
 
 
 def group_and_plot_pivot_graph(df, columns, filename):
@@ -55,6 +35,38 @@ def plot_bar_graph(df, column, filename):
 	plt.ylabel('count')
 	plt.tight_layout()
 	plt.savefig(filename)
+
+def transform_data_columns_1_4(dataframe):
+	#The county name column can be dropped cause it is completely null
+	dataframe=dataframe.drop(columns=['county_name'], axis=1, inplace=False)
+
+	# The gender dataset contains nulls. Here those nulls are discarded cause 
+	# the dataset is large and the nulls consitute about 5%. Also replacing the
+	# null data with frequent values might mislead the prediction
+	dataframe = dataframe.dropna(subset=['driver_gender'], inplace=False)
+
+	#Create a colum that gives only the year of stop, which can be used
+	#visualization of other columns with year
+	dataframe['stop_year'] = dataframe['stop_date'].apply(lambda x: int(x.split('-')[0]))
+  
+	return dataframe
+   
+    
+def visualize_data_columns_1_4(dataframe):  
+  #The statistics of the first four coumns are as follows
+
+  #Column 1 -> Stop Date - The year on which the person is stopped for each gender
+  group_and_plot_pivot_graph(dataframe, ['stop_year', 'driver_gender'], 'gender_with_year.png')
+
+
+  #Column 2 -> Stop Time - The hour on which the person is stopped for each gender
+  group_and_plot_pivot_graph(dataframe, ['stop_hour', 'driver_gender'], 'gender_with_hour.png')
+
+  #Column 3 -> Gender 
+  print(dataframe['driver_gender'].value_counts())
+  plot_pie_chart(dataframe,'driver_gender','driver_gender.png')
+
+  return dataframe
 
 
 def transform_data_columns_9_12(df): 
